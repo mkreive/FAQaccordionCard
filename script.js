ef4df74e589d741d851ecf403e49711afda78361
mkreive <mkreive@gmail.com>
2022-03-29 12:01:47 +0300

    svg done with links

--- script.js
+++ script.js
@@ -5,9 +5,12 @@ const questionElements = document.querySelectorAll(".questions__block");
 questionElements.forEach((question) => {
     question.addEventListener("click", function (e) {
         const clickedQuestion = e;
-        console.log(e.target);
+
         const answer = question.querySelector(".answer");
+        const svg = question.querySelector("svg");
+
         question.classList.toggle("active");
+        svg.classList.toggle("rotate");
 
         if (answer.style.display === "block") {
             answer.style.display = "none";
